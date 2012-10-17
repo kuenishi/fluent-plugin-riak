2a8de8b68ed8b99f7938b9336969c52ffea60c6f
UENISHI Kota <kuenishi@nausicaa>
2012-10-17 23:42:14 +0900

    todo: design key

--- lib/fluent/plugin/out_riak.rb
+++ lib/fluent/plugin/out_riak.rb
@@ -1,56 +1,85 @@
+module Fluent
 
-class RiakOutput < Fluent::BufferedOutput
+class RiakOutput < BufferedOutput
 
   Fluent::Plugin.register_output('riak', self)
+  include SetTimeKeyMixin
+  config_set_default :include_tag_key, false
+  include SetTagKeyMixin
+  config_set_default :include_time_key, true
 
-  config_param :nodes
+  config_param :nodes, :string, :default => "localhost:8081"
 
   def initialize
     super
     require 'riak'
     require 'msgpack'
-
   end
 
   def configure(conf)
     super
+
     @nodes = conf['nodes'].split(',').map{ |s|
       ip,port = s.split(':')
       {:host => ip, :pb_port => port.to_i}
     }
-    @queue = []
+    $log.debug "@nodes = #{@nodes}"
+
   end
 
   def start
-    @conn = Riak::Client.new(:nodes => @nodes,
-                             :protocol => "pbc")
+    $log.debug " => #{@buffer.chunk_limit} #{@buffer.queue_limit} "
+    @conn = Riak::Client.new(:nodes => @nodes, :protocol => "pbc")
     @bucket = @conn.bucket("fluentlog")
+    @buf = {}
   end
   def shutdown
     super
   end
+
   def format(tag, time, record)
-    [tag, time, record].to_msgpack
+    [time, record].to_msgpack
   end
 
   def emit(tag, es, chain)
+# #    $log.debug "#{@buffer} #{es}"
+    #es.to_msgpack
+#   end
     chain.next
     es.each { |time, record|
-      @queue << {time => record}
+      key = tag + "|" + time.to_s + "|" + @buf.size.to_s
+      @buf[key] = record
     }
 
-    now = Time.now.to_i.to_s
-    object = Riak::RObject.new(@bucket, now)
-    object.raw_data = @queue.to_json
-    object.content_type = 'text/json'
-    object.store
-    @queue = []
+    if @buf.size > @buffer.queue_limit then # FIXME: hand-made buffer
+      n = @buf.size
+      k = put_now(@buf)
+      @buf = {}
+      $log.debug "#{n} objects inserted with key=#{k}"
+    end
   end
 
   def write(chunk)
-    data = chunk.read
-    print data
-    p "============================================="
+    $log.warn " <<<<<===========\n"
+    # records  = []
+    # chunk.msgpack_each { |record|
+    #   records << record
+    # }
+    # $log.debug "#{records}"
   end
+
+  private
+
+  # TODO: add index for some analysis
+  def put_now(obj)
+    key = Time.now.to_i.to_s
+    obj = Riak::RObject.new(@bucket, key)
+    obj.raw_data = obj.to_json
+    obj.content_type = 'text/json'
+    obj.store
+    key
+  end
+
+end
 
 end
--- loader.rb
+++ loader.rb
@@ -1,9 +1,21 @@
 require 'net/http'
 
-uri = URI("http://192.168.62.128:8888/riak.test")
+# uri = URI("http://192.168.62.128:8888/riak.test")
 
-(1..1024*1024).each do |i|
-  json = "{\"hoge\":\"value\",\"id\":#{i}}"
-  res = Net::HTTP.post_form(uri, 'json' => json)
-  print "#{i}: #{res.code}\n"
+def run(n, uri)
+  (0..n).each do |i|
+    json = "{\"hoge\":\"valueoooooooo ???? special? \",\"id\":#{i}}"
+    res = Net::HTTP.post_form(uri, 'json' => json)
+    if (i % 1000) == 0 then
+      print "#{i}: #{res.code}\n"
+    end
+  end
 end
+
+uri = URI("http://192.168.100.128:8888/riak.test")
+n = 1024*1024
+start = Time.now
+run(n, uri)
+duration = (Time.now - start).to_i
+mps = n * 1.0 / duration
+print "#{n} messages in #{duration} secs: #{mps} mps.\n"
